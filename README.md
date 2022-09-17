963b552dcb2059e71845e09766ef70bf28c9d1e4
Leon Rinkel <leon@rinkel.me>
2022-09-17 13:34:36 +0200

    Do smth with the audio data

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # wave-stuff
-Read uncompressed WAVE files (.wav) in C
+
+This example code demonstrates how to read/write uncompressed WAVE files
+(`.wav`) in C. It reads an uncompressed 16 bit stereo WAVE file, applies `sin()`
+and `cos()` to the left and right channel respectively and outputs the resulting
+audio to a new file. The result sounds as if the audio source moves back and
+forth between the left and right channel.
 
 ```
-$ gcc -o wave wave.c && ./wave ../test.wav 
+$ gcc -o wave wave.c && ./wave out.wav test.wav
 chunk: id=RIFF, size=35586022
 riff.format=WAVE
 chunk: id=fmt , size=16
--- wave.c
+++ wave.c
@@ -39,48 +39,51 @@ struct wav_fmt
 int main(int argc, char* argv[])
 {
     int ret = EXIT_SUCCESS; /* return code to exit with */
-    const char* filename; /* filename of the wav file to open */
-    FILE* file = NULL; /* file descriptor of the wav file */
-    off_t off = 0; /* offset in the wav file */
+    const char* in_filename; /* filename of the input wav file */
+    const char* out_filename; /* filename of the output wav file */
+    FILE* in_file = NULL; /* file descriptor of the input wav file */
+    off_t off = 0; /* offset in the input wav file */
     size_t nread; /* bytes of data read */
-    struct wav_chunk chunk; /* chunk read from wav file */
-    struct wav_riff riff = {0}; /* riff chunk read from wav file */
-    struct wav_fmt fmt; /* fmt chunk read from wav file */
+    struct wav_chunk chunk; /* chunk read/written from/to wav file */
+    struct wav_riff riff = {0}; /* riff chunk read/written from/to wav file */
+    struct wav_fmt fmt; /* fmt chunk read/written from/to wav file */
     void* data = NULL; /* contents of data chunk */
     size_t data_size; /* size of data chunk */
     int nsamples; /* number of samples the file contains */
     int nseconds; /* length of file in seconds */
+    FILE* out_file = NULL; /* file descriptor of the output wav file */
 
     assert(sizeof(struct wav_chunk) == 8);
     assert(sizeof(struct wav_riff) == 12);
     assert(sizeof(struct wav_fmt) == 24);
 
     // parse cli args
-    if (argc != 2)
+    if (argc != 3)
     {
-        fprintf(stderr, "usage: wave <file>\n");
+        fprintf(stderr, "usage: wave <out> <in>\n");
         ret = EXIT_FAILURE;
         goto cleanup;
     }
+    out_filename = argv[1];
+    in_filename = argv[2];
 
-    // open the file
-    filename = argv[1];
-    file = fopen(filename, "r");
-    if (file == NULL)
+    // open the input file
+    in_file = fopen(in_filename, "r");
+    if (in_file == NULL)
     {
         perror("unable to open file");
         ret = EXIT_FAILURE;
         goto cleanup;
     }
 
-    // read file chunk by chunk
+    // read input file chunk by chunk
     while (
         riff.size == 0 || /* size of file not known yet or */
         off < riff.size + 8 /* offset not at the end yet */
     )
     {
         // seek in file
-        if (fseek(file, off, SEEK_SET) != 0)
+        if (fseek(in_file, off, SEEK_SET) != 0)
         {
             perror("unable to seek file");
             ret = EXIT_FAILURE;
@@ -88,7 +91,7 @@ int main(int argc, char* argv[])
         }
 
         // read next chunk from file
-        nread = fread(&chunk, sizeof(struct wav_chunk), 1, file);
+        nread = fread(&chunk, sizeof(struct wav_chunk), 1, in_file);
         if (nread != 1)
         {
             fprintf(stderr, "unable to read chunk");
@@ -104,7 +107,7 @@ int main(int argc, char* argv[])
             // this is a riff type chunk
 
             // go back to start of riff chunk
-            if (fseek(file, off, SEEK_SET) != 0)
+            if (fseek(in_file, off, SEEK_SET) != 0)
             {
                 perror("unable to seek file");
                 ret = EXIT_FAILURE;
@@ -112,7 +115,7 @@ int main(int argc, char* argv[])
             }
 
             // read riff chunk
-            nread = fread(&riff, sizeof(struct wav_riff), 1, file);
+            nread = fread(&riff, sizeof(struct wav_riff), 1, in_file);
             if (nread != 1)
             {
                 fprintf(stderr, "unable to read riff chunk");
@@ -130,7 +133,7 @@ int main(int argc, char* argv[])
             // this is a fmt type chunk
 
             // go back to start of fmt chunk
-            if (fseek(file, off, SEEK_SET) != 0)
+            if (fseek(in_file, off, SEEK_SET) != 0)
             {
                 perror("unable to seek file");
                 ret = EXIT_FAILURE;
@@ -138,7 +141,7 @@ int main(int argc, char* argv[])
             }
 
             // read fmt chunk
-            nread = fread(&fmt, sizeof(struct wav_fmt), 1, file);
+            nread = fread(&fmt, sizeof(struct wav_fmt), 1, in_file);
             if (nread != 1)
             {
                 fprintf(stderr, "unable to read fmt chunk");
@@ -178,7 +181,7 @@ int main(int argc, char* argv[])
             data_size = chunk.size;
 
             // read contents of data chunk
-            nread = fread(data, chunk.size, 1, file);
+            nread = fread(data, chunk.size, 1, in_file);
             if (nread != 1)
             {
                 fprintf(stderr, "unable to read contents of data chunk");
@@ -201,17 +204,66 @@ int main(int argc, char* argv[])
 
     LOG("nsamples=%d, nseconds=%d\n", nsamples, nseconds);
 
-    // TODO: do something with the data
+    // the following stuff is only implemented for 16 bit stereo pcm
+    if (
+        fmt.bits_per_sample != 16 ||
+        fmt.audio_format != 1 ||
+        fmt.num_channels != 2
+    )
+    {
+        fprintf(stderr, "unsupported format\n");
+        ret = EXIT_FAILURE;
+        goto cleanup;
+    }
+
+    /* do something with the data, in this example: apply sin() to left channel
+     * and cos() to right channel, so it sounds as if the audio source moves
+     * back and forth between the left and right channel */
+    for (int sample = 0; sample < nsamples; sample++)
+    {
+        // left and right channel are interleaved
+        int16_t* l = (((int16_t*) data) + 0 + sample * fmt.num_channels);
+        int16_t* r = (((int16_t*) data) + 1 + sample * fmt.num_channels);
+
+        // period is 2 seconds
+        double x = ((double) sample) / ((double) fmt.sample_rate * 2);
+        *l = ((double) *l) * sin(x * 2 * M_PI);
+        *r = ((double) *r) * cos(x * 2 * M_PI);
+    }
+
+    // open the output file
+    out_file = fopen(out_filename, "w");
+    if (out_file == NULL)
+    {
+        perror("unable to open file");
+        ret = EXIT_FAILURE;
+        goto cleanup;
+    }
+
+    // reuse chunk variable to mark data chunk
+    strncpy(chunk.id, "data", 4);
+    chunk.size = data_size;
+
+    // write the chunks to output file
+    fwrite(&riff, sizeof(struct wav_riff), 1, out_file);
+    fwrite(&fmt, sizeof(struct wav_fmt), 1, out_file);
+    fwrite(&chunk, sizeof(struct wav_chunk), 1, out_file);
+    fwrite(data, data_size, 1, out_file);
 
 cleanup:
+    if (out_file != NULL)
+    {
+        fclose(out_file);
+    }
+
     if (data != NULL)
     {
         free(data);
     }
 
-    if (file != NULL)
+    if (in_file != NULL)
     {
-        fclose(file);
+        fclose(in_file);
     }
     
     return ret;
